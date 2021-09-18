89474ea5d63642182a50b30fb8670506c8b752ce
Paul Fell <swellpf@gmail.com>
2021-09-18 17:38:24 -0400

    added useEffect and axios get call

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,13 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 
 const Search = () => {
     const [term, setTerm] = useState('');
+    const [results, setResults] = useState([]);
+
+    console.log(results);
+    
+    useEffect(() => {
+        const search = async () => {
+            const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: term,
+                }
+            });
+
+            setResults(data);
+        };
+        search();
+    }, [term]);
+
     return (
         <div>
             <div className="ui form">
                 <div className="field">
                     <label>Enter Search Term:</label>
-                    <input className="input" value={term} onChange={e => setTerm(e.target.value)} />
+                    <input className="input" value={term} onChange={(e) => setTerm(e.target.value)} />
                 </div>
             </div>
         </div>
