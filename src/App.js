ee7063446bbc66d336c773df4e50f6da59309d41
Paul Fell <swellpf@gmail.com>
2021-09-21 12:04:20 -0400

    Work on Route and Header components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
 import Translate from "./components/Translate";
+import Route from "./components/Route";
+import Header from "./components/Header";
 
 const items = [
     {
@@ -36,9 +38,24 @@ const options = [
 
 
 export default () => {
+    
+    
+    
     return (
         <div>
-            <Translate />
+            <Header />
+            <Route path="/">
+                <Accordion items={items} />
+            </Route>
+            <Route path="/list">
+                <Search />
+            </Route>
+            <Route path="/dropdown">
+                <Dropdown />
+            </Route>
+            <Route path="/translate">
+                <Translate />
+            </Route>
         </div>
     );
 };
